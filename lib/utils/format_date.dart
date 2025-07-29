b78cdc760152485103db4db42c14d6ca4e5c4ed4
akkarachai6787 <akkarachai6787@gmail.com>
2025-07-29 16:19:19 +0700

    feat: new utils

---
+++ lib/utils/format_date.dart
@@ -0,0 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+String formatDate(Timestamp timestamp) {
+    final date = timestamp.toDate();
+    return '${date.day}/${date.month}/${date.year} ${date.hour}:${date.minute.toString().padLeft(2, '0')}';
+  }
